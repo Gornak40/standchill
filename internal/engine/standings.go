b158759fae3afaeb141aca259a250f525716d213
Gornak40 <s-kozelsk@yandex.ru>
2024-10-27 14:56:19 +0300

    feat: working generator

--- internal/engine/standings.go
+++ internal/engine/standings.go
@@ -2,6 +2,9 @@ package engine
 
 import (
 	"bufio"
+	"encoding/csv"
+	"errors"
+	"io"
 	"log/slog"
 	"net/http"
 	"os/exec"
@@ -17,35 +20,57 @@ const (
 	secretLink = "https://algocourses.ru/standings/d2024_fall_e26d813e808378e66bc0a2c1?logins="
 )
 
+func (e *Engine) getLink(filter map[string]struct{}) (string, error) {
+	cmd := exec.Command("shoga", "-i", "55000")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", err
+	}
+	if err := cmd.Start(); err != nil {
+		return "", err
+	}
+
+	csvr := csv.NewReader(stdout)
+	csvr.Comma = ';'
+	link := secretLink
+	for {
+		rec, err := csvr.Read()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return "", err
+		}
+		if len(rec) < 3 {
+			slog.Warn("bad csv line", slog.Any("record", rec))
+			continue
+		}
+		login, name := rec[1], rec[2]
+		if _, ok := filter[login]; ok {
+			link += name + ","
+		}
+	}
+	if err := cmd.Wait(); err != nil {
+		return "", err
+	}
+	return strings.TrimRight(link, ","), nil
+}
+
 func (e *Engine) StandingsHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		logins := r.FormValue(loginsForm)
 		lr := bufio.NewScanner(strings.NewReader(logins))
-		good := make(map[string]struct{})
+		filter := make(map[string]struct{})
 		for lr.Scan() {
-			good[lr.Text()] = struct{}{}
+			filter[lr.Text()] = struct{}{}
 		}
-		cmd := exec.Command("shoga", "-i", "55000")
-		stdout, err := cmd.StdoutPipe()
+		link, err := e.getLink(filter)
 		if err != nil {
 			utils.Report(w, err)
 			return
 		}
-		if err := cmd.Start(); err != nil {
-			utils.Report(w, err)
-			return
-		}
-		csvr := bufio.NewScanner(stdout)
-		for csvr.Scan() {
-			slog.Info(csvr.Text())
-		}
-		if err := cmd.Wait(); err != nil {
-			utils.Report(w, err)
-			return
-		}
-
 		data := map[string]string{
-			"Link": strings.TrimRight(secretLink, ","),
+			"Link": link,
 		}
 		if err := e.tmpl.ExecuteTemplate(w, standingsTmpl, data); err != nil {
 			utils.Report(w, err)
